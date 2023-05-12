ab37dadbb3ecf29e6c15f8659a9cac8761ce37ca
weseonggu <fkskdldh@naver.com>
2023-05-12 16:48:26 +0900

    project start

---
+++ src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/MeoggolgolProjectApplication.java
@@ -0,0 +1,13 @@
+package com.soldesk.meoggolgol.MeoggolgolProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeoggolgolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeoggolgolProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soldesk/meoggolgol/MeoggolgolProject/MeoggolgolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soldesk.meoggolgol.MeoggolgolProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeoggolgolProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
