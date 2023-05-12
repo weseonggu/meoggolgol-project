487763b346d62db31e1bfa1d943039c49e15e9d6
ksw <sw6927@naver.com>
2023-05-12 16:57:24 +0900

    주석입니다

--- src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/MeoggolgolProjectApplication.java
+++ src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/MeoggolgolProjectApplication.java
@@ -2,7 +2,7 @@ package com.soldesk.meoggolgol.MeoggolgolProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// kdlfa;
 @SpringBootApplication
 public class MeoggolgolProjectApplication {
 
