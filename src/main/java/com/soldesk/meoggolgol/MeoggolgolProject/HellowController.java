dc47fb1bb6bcff2842e367e00aa96d207050018e
weseonggu <fkskdldh@naver.com>
2023-05-13 22:05:32 +0900

    jsp 사용준비

---
+++ src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/HellowController.java
@@ -0,0 +1,13 @@
+package com.soldesk.meoggolgol.MeoggolgolProject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HellowController {
+	@GetMapping("/")
+	public String hellow() {
+		return"hellow";
+	}
+}
