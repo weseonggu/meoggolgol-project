0a91ec604793f33829d5cd37f71f300c39917e10
ksw <sw6927@naver.com>
2023-05-17 10:42:39 +0900

    test

--- src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/HellowController.java
+++ src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/HellowController.java
@@ -3,7 +3,7 @@ package com.soldesk.meoggolgol.MeoggolgolProject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+// 아무거나
 @Controller
 public class HellowController {
 	@GetMapping("/")
