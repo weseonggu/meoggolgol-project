dde145eb7ea34a50e7170dff0d627ebefbe2d659
weseonggu <fkskdldh@naver.com>
2023-05-17 15:19:09 +0900

    메인 페이지에서 layout.hml파일 상속 하도록 변경

--- src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/HellowController.java
+++ src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/HellowController.java
@@ -2,12 +2,11 @@ package com.soldesk.meoggolgol.MeoggolgolProject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class HellowController {
 	@GetMapping("/")
-//	@ResponseBody
 	public String hellow() {
-		return"layout";
+		return"mainpage";
 	}
 }
