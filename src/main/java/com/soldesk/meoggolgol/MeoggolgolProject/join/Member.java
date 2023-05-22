45d00602ceecd745ce5eb7a4cbed6f2a8fc53510
ksw <sw6927@naver.com>
2023-05-22 09:40:46 +0900

    member 생성

---
+++ src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/join/Member.java
@@ -0,0 +1,20 @@
+package com.soldesk.meoggolgol.MeoggolgolProject.join;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+	private int member_index;
+	private String member_id;
+	private String member_pw;
+	private String member_nickname;
+	private Date member_birthday;
+	private String member_phoneNumber;
+	private String member_email;
+}
