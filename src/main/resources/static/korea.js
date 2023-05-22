e17c7179b27d7a8663d706a3c9f758110b471e01
DESKTOP-CTN34LM\user <tjdcksdl1997@naver.com>
2023-05-23 01:30:07 +0900

    지도에 마커 주변을 원으로 표시하여 시각적으로 확인할 수 있도록 코드작성

--- src/main/resources/static/korea.js
+++ src/main/resources/static/korea.js
@@ -136,23 +136,40 @@ window.onload = function() {
 
 
 	var container = document.getElementById('mggMap');
-		var options = {
-			center: new kakao.maps.LatLng(la, lo),
-			level: 3
-		};
+	var options = {
+	    center: new kakao.maps.LatLng(la, lo),
+ 	   level: 3
+	};
 
 	var map = new kakao.maps.Map(container, options);
 
-	//마커가 표시될 위치
-	var markerPosition  = new kakao.maps.LatLng(la, lo); 
-	
-	//마커를 생성
+	// 마커가 표시될 위치
+	var markerPosition = new kakao.maps.LatLng(la, lo);
+
+	// 마커를 생성
 	var marker = new kakao.maps.Marker({
-	    position: markerPosition
+ 	   position: markerPosition
 	});
-	
-	//마커가 지도 위에 표시되도록 설정
+
+	// 마커가 지도 위에 표시되도록 설정
 	marker.setMap(map);
+
+	// 지도에 표시할 원을 생성합니다
+	var circle = new kakao.maps.Circle({
+	    center: new kakao.maps.LatLng(la, lo), // 원의 중심좌표입니다
+	    radius: 500, // 미터 단위의 원의 반지름입니다
+	    strokeWeight: 5, // 선의 두께입니다
+		strokeColor: '#75B8FA', // 선의 색깔입니다
+ 	    strokeOpacity: 1, // 선의 불투명도입니다
+ 	    strokeStyle: 'dashed', // 선의 스타일입니다
+	    fillColor: '#CFE7FF', // 채우기 색깔입니다
+	    fillOpacity: 0.4 // 채우기 불투명도입니다
+	});
+
+	// 지도에 원을 표시합니다
+	circle.setMap(map);
+	
+	
 };
 // 시군도 선택시 그 지역의 먹자골목 리스트 출력
 function listAjax(code){
