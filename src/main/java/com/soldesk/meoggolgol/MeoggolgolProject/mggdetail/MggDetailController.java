b323ff4542802f0653b6439a8bc7a411fa2f5b5b
weseonggu <fkskdldh@naver.com>
2023-05-22 12:15:52 +0900

    먹골골 상세페이지 컨트롤러, 리포지터리, 서비스 클래스

---
+++ src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/mggdetail/MggDetailController.java
@@ -0,0 +1,8 @@
+package com.soldesk.meoggolgol.MeoggolgolProject.mggdetail;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MggDetailController {
+
+}
---
+++ src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/mggdetail/MggDetailRepository.java
@@ -0,0 +1,8 @@
+package com.soldesk.meoggolgol.MeoggolgolProject.mggdetail;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MggDetailRepository {
+
+}
