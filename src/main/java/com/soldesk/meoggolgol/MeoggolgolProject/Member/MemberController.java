c23feb4eb9dd7cf859869c80705319e85464fb6e
uznn <uznn0512@gmail.com>
2023-05-23 16:39:26 +0900

    뭐가 문젠지 모르게써요 ,,

--- src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/Member/MemberController.java
+++ src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/Member/MemberController.java
@@ -1,14 +1,23 @@
+ 
 package com.soldesk.meoggolgol.MeoggolgolProject.Member;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@SessionAttributes("member_id")
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -45,5 +54,30 @@ public class MemberController {
 		System.out.println(member);
 		return "redirect:/";
 	}
+	
+	@GetMapping("/signin")
+	public String goSignIn(Member member) {
+	    return "signInForm";
+	}
+
+	@PostMapping("/signin")
+	public String login(@Valid Member member, BindingResult bindingResult) {
+	    
+	    if (bindingResult.hasErrors()) {
+	        return "signInForm";
+	    }
+	    
+	    if (reposi.check(member.getMember_id(), member.getMember_pw()) == null) {
+	        bindingResult.rejectValue("member_id", "invalidCredentials", "아이디 또는 패스워드가 일치하지 않습니다.");
+	        return "signInForm";
+	    }
+	    
+	    // 로그인 성공
+	    return "redirect:/mainpage";
+	}
+
+	
+
 
 }
+ 
