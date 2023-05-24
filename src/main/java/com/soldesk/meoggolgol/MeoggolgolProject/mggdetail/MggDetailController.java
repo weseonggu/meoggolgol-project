6816566fce632235ff29a13a2a0deeff5a3a8655
ksw <sw6927@naver.com>
2023-05-24 09:44:18 +0900

    상세 페이지 수정

--- src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/mggdetail/MggDetailController.java
+++ src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/mggdetail/MggDetailController.java
@@ -18,11 +18,15 @@ public class MggDetailController {
         mggDetailService.searchRestaurants(la, lo);	//mggDetailService로 la, lo 넘겨주기
         SelectMgg selectmgg = mdr.check(lo, la);
         //System.out.println(selectmgg);
+        String parking = "없음";
+        if (selectmgg.getPARKNG_POSBL_AT().equals("유")) {
+			parking = "있음";
+		}
         
         model.addAttribute("fclty_nm", selectmgg.getFCLTY_NM());
         model.addAttribute("rdnmadr_nm", selectmgg.getRDNMADR_NM());
         model.addAttribute("nearby_pbtrnsp_nm", selectmgg.getNEARBY_PBTRNSP_NM());
-        model.addAttribute("parkng_posbl_at", selectmgg.getPARKNG_POSBL_AT());
+        model.addAttribute("parkng_posbl_at", parking);
         
         model.addAttribute("la", la);
         model.addAttribute("lo", lo);
