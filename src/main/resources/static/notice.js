9d6f9b2f0bb905728ebdf66aaab8959e35660871
weseonggu <fkskdldh@naver.com>
2023-06-05 11:11:42 +0900

    no message

--- src/main/resources/static/notice.js
+++ src/main/resources/static/notice.js
@@ -1,5 +1,4 @@
 function deleteNotice(){
-	var id = $("#notice_num").value();
-	alert(id);
-//	location.href="/notice/detail/" + id + "/delete";
+	var id = $("#notice_num").text();
+	location.href="/notice/detail/" + id + "/delete";
 }
