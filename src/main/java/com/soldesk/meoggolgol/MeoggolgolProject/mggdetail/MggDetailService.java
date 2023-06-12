0b5555683151158abaac871821f84faa54c4ba7d
weseonggu <fkskdldh@naver.com>
2023-06-12 16:59:49 +0900

    커밋

--- src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/mggdetail/MggDetailService.java
+++ src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/mggdetail/MggDetailService.java
@@ -12,7 +12,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Service;
 
-import com.soldesk.meoggolgol.MeoggolgolProject.crawling.Selenium;
 import com.soldesk.meoggolgol.MeoggolgolProject.crawling.SeleniumSingleton;
 
 import lombok.RequiredArgsConstructor;
