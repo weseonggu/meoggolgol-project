b943b9a932d8bea4d890016e98142af3208a7b37
DESKTOP-CTN34LM\user <tjdcksdl1997@naver.com>
2023-06-13 21:49:21 +0900

    html의 리뷰 내용을 읽지 못하는 거 같아서 이것저것 해본 코드들입니다. 아직 해보는 중이지만 일단 올려드려요.

--- src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/RestaurantPage/MggRestController.java
+++ src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/RestaurantPage/MggRestController.java
@@ -1,20 +1,12 @@
 package com.soldesk.meoggolgol.MeoggolgolProject.RestaurantPage;
 
-import java.time.LocalDate;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.soldesk.meoggolgol.MeoggolgolProject.Member.MemberSignIn;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -39,17 +31,27 @@ public class MggRestController {
 		model.addAttribute("review", mrr.getReviewInfo(placeName));
 	    return "restaurant-detail";
 	}
-	
-	// 음식점 리뷰 등록 요청
-	@PostMapping("/restaurant-detail/review")
-	public String writeReview(@RequestBody ReviewRequest reviewRequest) {
-//		@RequestBody ReviewRequest reviewrequest
-		System.out.println(reviewRequest.getContent());
-		return "성공";
-
+	@GetMapping("/restaurant-detail/review")
+	public ModelAndView writeReview(@RequestParam(required = false) Double lo, @RequestParam(required = false) Double la, @RequestParam String imgUrl, @RequestParam String placeUrl, @RequestParam String placeName, @RequestParam String roadAddress, Model model, @ModelAttribute("reviewRequest") ReviewRequest reviewRequest) {
+	    if (lo == null || la == null) {
+	        // lo와 la 값이 없는 경우에 대한 처리
+	        // 예를 들어, 기본 값을 설정하거나 오류 메시지를 반환할 수 있습니다.
+	    } else {
+	        // lo와 la 값이 있는 경우에 대한 처리
+	        System.out.println(reviewRequest.getContent());
+	    }
+	    ModelAndView modelAndView = new ModelAndView("성공");
+	    return modelAndView;
 	}
-
 
 
 	
 }
+//	// 음식점 리뷰 등록 요청
+//	@GetMapping("/restaurant-detail/review")
+//	public String writeReview(@RequestBody ReviewRequest reviewRequest) {
+////		@RequestBody ReviewRequest reviewrequest
+//		System.out.println(reviewRequest.getContent());
+//		return "성공";
+//
+//	}
