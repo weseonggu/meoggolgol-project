a568e6ca94ab86c52b778e2883ea1c01c140f429
DESKTOP-CTN34LM\user <tjdcksdl1997@naver.com>
2023-06-15 16:08:21 +0900

    테이블 목록 간의 패딩 추가 및 오타 수정

--- src/main/resources/static/korea.js
+++ src/main/resources/static/korea.js
@@ -194,16 +194,22 @@ function listAjax(code){
 		if(data != null && data.length > 0){
             $("#meoggolgolHead").empty().append(
                 $("<tr></tr>").append(
-                    $("<th></th>").text("목자골목"),
+                    $("<th></th>").text("먹자골목"),
                     $("<th></th>").text("주소")
                 )
             );
             $.each(data, function(i) {
-                var ntd = $("<td></td>").text(data[i].FCLTY_NM);
-                var ltd = $("<td></td>").text(data[i].RDNMADR_NM);
-                var lotd = $("<input>").attr("type", "hidden").attr("value", data[i].FCLTY_LO);
-                var latd = $("<input>").attr("type", "hidden").attr("value", data[i].FCLTY_LA);
-                
+    			var ntd = $("<td></td>").text(data[i].FCLTY_NM).css({
+        			"padding-bottom": "20px",
+        			"padding-top": "20px"
+    			});
+    			var ltd = $("<td></td>").text(data[i].RDNMADR_NM).css({
+        			"padding-bottom": "20px",
+        			"padding-top": "20px"
+    			});
+    			var lotd = $("<input>").attr("type", "hidden").attr("value", data[i].FCLTY_LO);
+    			var latd = $("<input>").attr("type", "hidden").attr("value", data[i].FCLTY_LA);
+
                 ntd.hover(
                     function() {
                         $(this).css("color", "#F06292"); // 마우스를 올렸을 때 배경색을 노란색으로 변경
