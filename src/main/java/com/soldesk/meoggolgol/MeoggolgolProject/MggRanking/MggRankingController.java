76b5dd436e3d1a3f6da9bae9ba680ca1aca72595
ksw <sw6927@naver.com>
2023-06-16 16:24:56 +0900

    랭킹

--- src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/MggRanking/MggRankingController.java
+++ src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/MggRanking/MggRankingController.java
@@ -1,5 +1,22 @@
 package com.soldesk.meoggolgol.MeoggolgolProject.MggRanking;
 
-public class MggRankingController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MggRankingController {
+	private final MggRankingRepository mrr;
+	
+	// 음식점 상세 페이지 요청
+		@GetMapping("/mggRanking")
+		public String goRestPage(Model model) {
+
+			model.addAttribute("rank", mrr.getRanking());
+			
+		    return "mggRanking";
+		}
 }
--- src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/MggRanking/MggRankingRepository.java
+++ src/main/java/com/soldesk/meoggolgol/MeoggolgolProject/MggRanking/MggRankingRepository.java
@@ -1,5 +1,31 @@
 package com.soldesk.meoggolgol.MeoggolgolProject.MggRanking;
 
-public class MggRankingRepository {
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class MggRankingRepository {
+private final JdbcTemplate jdbc;
+	
+	private static String SELECT_RANKING=
+			"""
+			SELECT rr_mggname, average_score, RANK() OVER (ORDER BY average_score DESC) AS ranking
+			FROM (
+			    SELECT rr_mggname, AVG(rr_score) AS average_score
+			    FROM restaurantreview
+			    GROUP BY rr_mggname
+			) AS subquery
+			ORDER BY ranking ASC
+			LIMIT 9;
+			""";
+	
+	public List<Map<String,Object>> getRanking(){
+		return jdbc.queryForList(SELECT_RANKING);
+	}
 }
